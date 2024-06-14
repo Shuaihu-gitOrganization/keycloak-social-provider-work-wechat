d9a748006f911524089b98b29e81a7764fbb842a
shuaihu.zhang <shuaihu.zhang@9fzt.com.cn>
2024-06-14 19:06:47 +0800

    style(WechatWorkProviderConfig):格式化代码

--- src/main/java/org/keycloak/social/wechat/WechatWorkProviderConfig.java
+++ src/main/java/org/keycloak/social/wechat/WechatWorkProviderConfig.java
@@ -14,7 +14,6 @@ public class WechatWorkProviderConfig extends OAuth2IdentityProviderConfig {
   private static final Logger log = Logger.getLogger(WechatWorkProviderConfig.class);
   public WechatWorkProviderConfig(IdentityProviderModel model) {
     super(model);
-    log.info("WechatWorkProviderConfig"+model);
   }
 
   public WechatWorkProviderConfig() {
@@ -22,22 +21,18 @@ public class WechatWorkProviderConfig extends OAuth2IdentityProviderConfig {
   }
 
   public String getAgentId() {
-    log.info("getAgentId"+getConfig().get("agentId"));
     return getConfig().get("agentId");
   }
 
   public void setAgentId(String agentId) {
-    log.info("setAgentId"+agentId);
     getConfig().put("agentId", agentId);
   }
 
   public String getQrcodeAuthorizationUrl() {
-    log.info("getQrcodeAuthorizationUrl"+getConfig().get("qrcodeAuthorizationUrl"));
     return getConfig().get("qrcodeAuthorizationUrl");
   }
 
   public void setQrcodeAuthorizationUrl(String qrcodeAuthorizationUrl) {
-    log.info("setQrcodeAuthorizationUrl"+qrcodeAuthorizationUrl);
     getConfig().put("qrcodeAuthorizationUrl", qrcodeAuthorizationUrl);
   }
 
